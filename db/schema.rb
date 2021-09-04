4d98fc45828de1ec1878f4b16d1ca3bf90592f8b
macroshin <sckm0315@gmail.com>
2021-09-04 03:22:18 +0000

    [Add] ordersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_030658) do
+ActiveRecord::Schema.define(version: 2021_09_04_031933) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -74,6 +74,19 @@ ActiveRecord::Schema.define(version: 2021_09_04_030658) do
     t.text "introduction", null: false
     t.integer "price", null: false
     t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.integer "shopping_cost", null: false
+    t.integer "total_payment", null: false
+    t.integer "payment_method", default: 0, null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
