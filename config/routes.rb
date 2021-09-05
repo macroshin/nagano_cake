053394d327e1077cee6eb2e6d998ef52525d2164
macroshin <sckm0315@gmail.com>
2021-09-05 12:10:44 +0000

    [Add] adminのcontroller,viewのファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
+    root to: 'homes#top'
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    resources :order_details, only:[:update]
+    resources :orders, only:[:update]
+  end
 end
