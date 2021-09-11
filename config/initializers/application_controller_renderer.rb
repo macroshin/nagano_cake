44e54d32ed426f5f6ea919fe9066e1792909b642
macroshin <sckm0315@gmail.com>
2021-09-11 05:36:37 +0000

    [Add] admin/items show内記入

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '80d9720d6080ba0804422f1bea9080f4a6136378acf4f843e14113ec76ba72b22b939f9b71c813386967ecd17b52a0ea9467ac7b25b1526fcec94d0b673dc8b8'
