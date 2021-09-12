4421b436071c3eb72da729b04bb1109c1a7cea36
macroshin <sckm0315@gmail.com>
2021-09-12 07:16:58 +0000

    [Add] admin/customer show追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,12 +4,19 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_passward, :postal_code, :address, :telephone_number, :is_active, :created_at, :updated_at)
   end
 
 end
